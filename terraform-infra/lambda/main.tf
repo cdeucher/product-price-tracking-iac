ea2012692412e5e99d6d06bb091b65f1734c0fe7
Cristiano Deucher <1205053+cdeucher@users.noreply.github.com>
2022-12-02 21:53:27 -0300

    feat: add lambda layer

--- terraform-infra/lambda/main.tf
+++ terraform-infra/lambda/main.tf
@@ -1,14 +1,25 @@
 data "archive_file" "code" {
     type        = "zip"
-    source_dir  = var.src_path
-    output_path = "outputs/${var.src_path}.zip"
+    source_dir  = "${var.src_path}/src"
+    output_path = "outputs/${var.src_path}-src.zip"
 }
-
+data "archive_file" "layer" {
+    type        = "zip"
+    source_dir  = "${var.src_path}/layer"
+    output_path = "outputs/${var.src_path}-layer.zip"
+}
+resource "aws_lambda_layer_version" "this" {
+    filename    = "outputs/${var.src_path}-layer.zip"
+    layer_name  = "${var.project}-${var.function_name}-layer"
+    description = "${var.project}-${var.function_name}-layer"
+    compatible_runtimes = ["python3.8"]
+}
 resource "aws_lambda_function" "lambda" {
     function_name    = "${var.project}-${var.function_name}"
     role             = aws_iam_role.role_for_lambda.arn
     runtime          = "python3.8"
     filename         = data.archive_file.code.output_path
+    layers           = [aws_lambda_layer_version.this.arn]
     handler          = "app.handle"
     source_code_hash = data.archive_file.code.output_base64sha256
     publish          = true
